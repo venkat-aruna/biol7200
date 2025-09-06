0957d30acd23951cd422ec9d3d7df03d91ad6e2a
venkat-aruna <aruna03venkat@gmail.com>
2025-09-06 19:38:44 -0400

    Add: Filter output and print matches

--- find_homologs.sh
+++ find_homologs.sh
@@ -8,3 +8,6 @@ $of_name=$3
 #tblastn is from protein -> nucleotide db
 tblastn -query "$qf_name" -subject "$sf_name" -outfmt "6 pident qlen length" -out matches.txt
 
+#filter output
+awk '{if ($1>30 && $3>($4*0.9) print $0}' matches.txt > "$of_name"
+wc -l $of_name
