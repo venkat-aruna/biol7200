0b762f0c3e382c2376e2c85a6a10ee0c9a951fae
venkat-aruna <aruna03venkat@gmail.com>
2025-09-07 19:48:17 -0400

    Bug Fixed! Change in columns

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #find_homologs.sh <query file> <subject file> <output file
-$qf_name=$1
-$sf_name=$2
-$of_name=$3
+qf_name=$1
+sf_name=$2
+of_name=$3
 
 #tblastn is from protein -> nucleotide db
-tblastn -query "$qf_name" -subject "$sf_name" -outfmt "6 pident qlen length" -out matches.txt
+tblastn -query "$qf_name" -subject "$sf_name" -outfmt "6 sseqid pident qlen length" -out matches.txt
 
 #filter output
-awk '{if ($1>30 && $3>($4*0.9) print $0}' matches.txt > "$of_name"
-wc -l $of_name
+awk '{if ($2>30 && $4>($3*0.9)) print $0}' matches.txt | wc -l 
+awk '{if ($2>30 && $4>($3*0.9)) print $0}' matches.txt > $of_name
 
-#END
